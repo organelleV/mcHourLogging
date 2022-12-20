24727311f15149ac7bab8f6ee16043ee6bc27c0c
Uten <50422375+organelleV@users.noreply.github.com>
2022-12-19 22:57:53 -0500

    Update count when server is running and players>0

--- dataCollector.py
+++ dataCollector.py
@@ -4,27 +4,28 @@ import mcServerStatus
 import os
 
 def save_player_count(player_count):
-    # Get the current timestamp
-    timestamp = datetime.datetime.now()
+    print('player_count: '+str(player_count))
+    if (player_count!=None and player_count>0): # save player counts greater than 0
+        # Get the current timestamp
+        timestamp = datetime.datetime.now()
 
-    # Check if the file exists and has data
-    file_exists = os.path.exists('player_counts.csv') and os.path.getsize('player_counts.csv') > 0
+        # Check if the file exists and has data
+        file_exists = os.path.exists('player_counts.csv') and os.path.getsize('player_counts.csv') > 0
 
-    # Save the player count and timestamp to a file or database
-    with open('player_counts.csv', 'a') as f:
-        # Write the column headers if the file is empty
-        if not file_exists:
-            f.write(f"timestamp,player_count\n")
+        # Save the player count and timestamp to a file or database
+        with open('player_counts.csv', 'a') as f:
+            # Write the column headers if the file is empty
+            if not file_exists:
+                f.write(f"timestamp,player_count\n")
 
-        # Write the data for the current run
-        f.write(f"{timestamp},{player_count}\n")
+            # Write the data for the current run
+            f.write(f"{timestamp},{player_count}\n")
 
 
-player_count = mcServerStatus.getNumPlayers()
-save_player_count(player_count)
 while True:
     # Get the current time
     now = datetime.datetime.now()
+    print("hour: "+str(now.hour))
 
     # Check if the current time is within the desired time range (e.g., 8:00 AM to 8:00 PM)
     if now.hour >= 8 and now.hour < 20:
