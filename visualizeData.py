52f621b514d4e1b45538010ce5aab95a776b6c6f
Uten <50422375+organelleV@users.noreply.github.com>
2022-12-21 23:11:46 -0500

    switched to using plottly

--- visualizeData.py
+++ visualizeData.py
@@ -1,77 +1,54 @@
+from datetime import datetime
 import matplotlib.pyplot as plt
 import pandas as pd
+import csv
+import plotly.express as px
 
-def visualize_player_count_line_chart(data):
+def string_to_datetime(string):
+    return datetime.strptime(string, '%Y-%m-%d %H:%M:%S.%f')
+
+def print_data(data):
+    for date,players in data:
+        print("\nDay:",date.day,"Hour:",date.hour,"Players:",players)
+
+def visualize_player_count(data):
     # Load the data into a Pandas DataFrame
-    df = pd.read_csv(data)
+    csvreader = csv.reader(open(data))
+    header = []
+    header = next(csvreader)
+    rows = []
+    for row in csvreader:
+        row[0] = string_to_datetime(row[0])
+        rows.append(row)
+    print_data(rows)
+    
+    #sort into days
+    days = {}
+    numplayers_template = [0,0,0,0,0,0,0,0,0,0,0,0,0]
+    hours_template = [8,9,10,11,12,13,14,15,16,17,18,19,20]
+    # fig = px.line(x=hours_template,y=numplayers_template)
+    # fig.show()
+    # fig2 = px.line(x=hours_template,y=numplayers_template)
+    # fig2.show()
 
-    # Convert the timestamp column to a datetime index
-    df.index = pd.to_datetime(df['timestamp'])
-
-    # Format the datetime index as "YYYY-MM-DD HR:MIN"
-    df.index = df.index.strftime('%m-%d %H:%M')
-
-    # Plot the player count data as a line chart
-    plt.plot(df['player_count'])
-    plt.xlabel('Time')
-    plt.ylabel('Number of Players')
-    plt.title('Player Count Over Time')
-    plt.show()
-
-def visualize_player_count_bar_chart(data):
-    # Load the data into a Pandas DataFrame
-    df = pd.read_csv(data)
-
-    # Convert the timestamp column to a datetime index
-    df.index = pd.to_datetime(df['timestamp'])
-
-    # Group the data by day and take the maximum player count
-    df_by_day = df.resample('D').max()
-
-    # Filter out days with no players
-    df_by_day = df_by_day[df_by_day['player_count'] > 0]
-
-    # Keep only the last 7 days of data
-    df_by_day = df_by_day.tail(7)
-
-    # Format the datetime index as "YYYY-MM-DD"
-    df_by_day.index = df_by_day.index.strftime('%Y-%m-%d')
-
-    # Plot the player count data as a bar chart
-    plt.bar(df_by_day.index, df_by_day['player_count'])
-    plt.xlabel('Time')
-    plt.ylabel('Number of Players')
-    plt.title('Maximum Player Count by Day (Last 7 Days)')
-    plt.show()
-
-def visualize_player_count_hour_bar_chart(data):
-    # Load the data into a Pandas DataFrame
-    df = pd.read_csv(data)
-
-    # Convert the timestamp column to a datetime index
-    df.index = pd.to_datetime(df['timestamp'])
-
-    # Group the data by hour and take the mean
-    df_by_hour = df.resample('H').mean()
-
-    # Filter out hours with no players
-    df_by_hour = df_by_hour[df_by_hour['player_count'] > 0]
-
-    # Keep only the last 7 days of data
-    df_by_hour = df_by_hour.tail(168)
-
-    # Format the datetime index as "YYYY-MM-DD HR"
-    df_by_hour.index = df_by_hour.index.strftime('%Y-%m-%d %H')
-
-    # Plot the player count data as a bar chart
-    plt.bar(df_by_hour.index, df_by_hour['player_count'])
-    plt.xlabel('Time')
-    plt.ylabel('Number of Players')
-    plt.title('Player Count by Hour (Last 7 Days)')
-    plt.show()
+    for date,players in rows:
+        hour = date.hour
+        hourIndex = hour-8
+        players = int(players)
+        day = str(date.year)+"/"+str(date.month)+"/"+str(date.day)
+        if day not in days:
+            days[day] = numplayers_template.copy()
+        if players>days[day][hourIndex]:days[day][hourIndex]=players
+    
+    print(days)
+    for dayNum in days:
+        fig = px.line(x=hours_template,y=days[dayNum],title="Minecraft Server, Player Activity on "+dayNum)
+        #fig.
+        fig.show()
 
 
 
+    
 
-visualize_player_count_hour_bar_chart('player_counts.csv')
-
+# Example usage:
+visualize_player_count('player_counts.csv')
